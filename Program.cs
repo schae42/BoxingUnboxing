db1021f68befa9825fda45398373370f664574c4
schae42 <chaechan@outlook.com>
2020-12-01 13:44:39 -0800

    boxingUnboxing

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objList = new List<object>
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+
+            foreach (var o in objList)
+            {
+                Console.WriteLine(o);
+            }
+
+            int sum = 0;
+            foreach (var o in objList)
+            {
+                if (o is int)
+                {
+                    sum += (int)o;
+                }
+
+
+            }
+                Console.WriteLine(sum);
+
+        }
+    }
+}
+
